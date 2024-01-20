bd82c331388f77329acb39b46a389212d57fe8e7
yuval14vaknin <yuvalvaknin@gmail.com>
2024-01-20 21:31:46 +0200

    swagger

--- src/api/comments/commentRoute.ts
+++ src/api/comments/commentRoute.ts
@@ -3,8 +3,92 @@ import { createComment, getCommentsByPost, getCommentsNumberPerPost } from "./co
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /postComment:
+ *   post:
+ *     summary: Post a comment
+ *     description: Create and post a new comment with the provided data.
+ *     requestBody:
+ *       description: Comment data
+ *       required: true
+ *       content:
+ *         application/json:
+ *           example: { postId: 'postId', userName: 'JohnDoe', content: 'This is a great post!' }
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             example: { _id: 'commentId', postId: 'postId', userName: 'JohnDoe', content: 'This is a great post!' }
+ *       500:
+ *         description: Internal Server Error
+ *         content:
+ *           text/plain:
+ *             example: Internal Server Error
+ *     tags:
+ *      - comments
+ */
 router.post('/postComment', createComment);
+
+/**
+ * @swagger
+ * /commentsCounter/{postId}:
+ *   get:
+ *     summary: Get comments count for a post
+ *     description: Retrieve the number of comments for a specific post.
+ *     parameters:
+ *       - in: path
+ *         name: postId
+ *         description: ID of the post to get comments count
+ *         required: true
+ *         schema:
+ *           type: string
+ *           example: postId
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             example: 5
+ *       500:
+ *         description: Internal Server Error
+ *         content:
+ *           text/plain:
+ *             example: Internal Server Error
+ *     tags:
+ *      - comments
+ */
 router.get('/commentsCounter/:postId', getCommentsNumberPerPost);
+
+/**
+ * @swagger
+ * /getCommentsByPost/{postId}:
+ *   get:
+ *     summary: Get comments by post ID
+ *     description: Retrieve comments associated with a specific post by post ID.
+ *     parameters:
+ *       - in: path
+ *         name: postId
+ *         description: ID of the post to get comments for
+ *         required: true
+ *         schema:
+ *           type: string
+ *           example: postId
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             example: [{ _id: 'commentId1', postId: 'postId', userName: 'JohnDoe', content: 'Great post!' }, { _id: 'commentId2', postId: 'postId', userName: 'JaneDoe', content: 'Nice post!' }]
+ *       500:
+ *         description: Internal Server Error
+ *         content:
+ *           text/plain:
+ *             example: Internal Server Error
+ *     tags:
+ *      - comments
+ */
 router.get('/getCommentsByPost/:postId', getCommentsByPost);
 
 export default router;
--- src/api/messages/messageRoute.ts
+++ src/api/messages/messageRoute.ts
@@ -3,7 +3,63 @@ import { createNewMessage, getAllMessageBetweenTwoUsers } from "./messageHandler
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /sendMessage:
+ *   post:
+ *     summary: Send a message
+ *     description: Create and send a new message with the provided data.
+ *     requestBody:
+ *       description: Message data
+ *       required: true
+ *       content:
+ *         application/json:
+ *           example: { fromUser: 'JohnDoe', toUser: 'JaneDoe', messageContent: 'Hello, Jane!' }
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             example: { _id: 'messageId', fromUser: 'JohnDoe', toUser: 'JaneDoe', messageContent: 'Hello, Jane!' }
+ *       500:
+ *         description: Internal Server Error
+ *         content:
+ *           text/plain:
+ *             example: Internal Server Error
+ *     tags:
+ *      - messages
+ */
 router.post('/sendMessage', createNewMessage);
+
+/**
+ * @swagger
+ * /getChat:
+ *   post:
+ *     summary: Get chat messages between two users
+ *     description: Retrieve chat messages between two specified users.
+ *     requestBody:
+ *       description: Users for which to fetch the chat
+ *       required: true
+ *       content:
+ *         application/json:
+ *           example: { firstUser: 'JohnDoe', secondUser: 'JaneDoe' }
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             example: [
+ *               { _id: 'messageId1', fromUser: 'JohnDoe', toUser: 'JaneDoe', content: 'Hello, Jane!' },
+ *               { _id: 'messageId2', fromUser: 'JaneDoe', toUser: 'JohnDoe', content: 'Hi, John!' }
+ *             ]
+ *       500:
+ *         description: Internal Server Error
+ *         content:
+ *           text/plain:
+ *             example: Internal Server Error
+ *     tags:
+ *      - messages
+ */
 router.post('/getChat', getAllMessageBetweenTwoUsers);
 
 export default router;
