e236de8405a7db603a4929ecc846ca46e19c1a54
Omri Alster <omrialster@gmail.com>
2024-01-26 13:03:42 -0800

    change to userId in post

--- src/api/posts/postRouter.ts
+++ src/api/posts/postRouter.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { createPost, updatePost, deletePost, findAll, getPostsByCountry, getPostsByUserName  } from './postHandlers';
+import { authenticate } from '../../middlewares';
 
 const router = express.Router();
 
@@ -51,7 +52,7 @@ router.get('/', findAll);
  *     tags:
  *      - posts
  */
-router.post('/', createPost);
+router.post('/', authenticate, createPost);
 
 /**
  * @swagger
